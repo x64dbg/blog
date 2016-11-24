9e456677b02f24582a3e489ab57d250b0136da56
mrexodia <mr.exodia.tpodt@gmail.com>
2016-11-24 17:48:20 +0100

    large image fade

---
+++ public/js/image.js
@@ -0,0 +1,15 @@
+function imageClick() {
+    $("#largeImage").html("<img src=\"" + $(this).attr("src") + "\" alt=\"" + $(this).attr("alt") + "\">");
+    $("#largeImage").fadeIn(800);
+    $("#largeImage img").fadeIn(1000);
+}
+
+function largeImageClick() {
+    $("#largeImage").fadeOut(1000);
+    $("#largeImage img").fadeOut(500);
+}
+
+$(document).ready(function() {
+    $(".post img").click(imageClick);
+    $("#largeImage").click(largeImageClick);
+}
